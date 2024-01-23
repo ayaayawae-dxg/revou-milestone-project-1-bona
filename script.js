987375864560ee4a3770cac4c7ade49075c6a473
Bonaventura Sanjaya <bonaventura.sanjaya@dexagroup.com>
2024-01-23 13:39:25 +0700

    responsive nav menu

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const isMobile = window.innerWidth < 768
+const toggleNavButtons = document.getElementsByClassName('toggle-nav')
+const navMenu = document.getElementById('nav-menu')
+const navWidth = getComputedStyle(document.documentElement).getPropertyValue('--nav-menu-width')
+const learningContentWrapper = document.getElementById('learning-content')
+
 const init = () => {
     console.log('Loaded')
+    if (!isMobile) {
+        navMenu.classList.add('is-active')
+    }
+}
+
+// nav menu listener
+for (let toggleBtn of toggleNavButtons) {
+    toggleBtn.addEventListener('click', () => {
+        const isNavActive = navMenu.className.split(' ').includes('is-active')
+        navMenu.classList.toggle("is-active");
+        if (!isNavActive) {
+            if (!isMobile) {
+                learningContentWrapper.style.marginLeft = navWidth
+            }
+        } else {
+            learningContentWrapper.style.marginLeft = '0px'
+        }
+    })
 }
